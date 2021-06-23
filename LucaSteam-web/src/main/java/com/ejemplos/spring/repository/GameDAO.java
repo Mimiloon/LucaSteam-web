8b0a68a78f4b5ca0cf3815ae9bfb79b22fdb99ba
anadm21 <annatrabajos2@gmail.com>
2021-06-23 19:35:48 +0200

    Paquetes service, controller, repository, clase Game, BBDD

---
+++ LucaSteam-web/src/main/java/com/ejemplos/spring/repository/GameDAO.java
@@ -0,0 +1,17 @@
+package com.ejemplos.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ejemplos.spring.model.Game;
+
+@Repository
+public interface GameDAO extends JpaRepository<Game,Integer> {
+
+	@Query("SELECT name FROM vgsales WHERE name =?1")
+	List<Game> findByName(String name);
+
+}
